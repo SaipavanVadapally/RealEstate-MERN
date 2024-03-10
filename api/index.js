92300c1e78f9f6a28fb1ffcaec9cc7f753edbf0c
Saipavanvadapally <pavanvadapally034@gmail.com>
2024-03-10 19:50:42 +0530

    connected to MONGODB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,24 @@
 import express from 'express';
-const app = express();
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000!');
-});
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Access the MongoDB URI from the environment variable
+const mongoUri = process.env.MONGO;
+
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB!!");
+    
+    // Your MongoDB-connected code can go here
+    
+    const app = express();
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000!!!!');
+    });
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB:", err);
+  });
