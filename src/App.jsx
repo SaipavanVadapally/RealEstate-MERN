b45c0ab89fe6444ddf1581131b2dbea69708458c
Saipavanvadapally <pavanvadapally034@gmail.com>
2024-03-10 10:34:46 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
-import React from "react";
 
+import { BrowserRouter,Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
 export default function App() {
-  return <h1 className="text-red-500">UrbanScape Residences</h1>;
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/sign-in" element={<SignIn/>}/>
+    <Route path="/sign-up" element={<SignUp/>}/>
+    <Route path="/profile" element={<Profile/>}/>
+    <Route path="/about" element={<About/>}/>
+ 
+  </Routes>
+   </BrowserRouter>;
 
 }
