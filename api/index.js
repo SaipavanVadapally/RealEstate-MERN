3fba0ea8e840f724442cab30217bbf15fdd1de4a
Saipavanvadapally <pavanvadapally034@gmail.com>
2024-03-11 12:34:49 +0530

    created signup api route

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import UserRouter from './routes/user.route.js';  // Make sure to adjust the path to your user.route.js file
-
+import authRouter from './routes/auth.route.js';
 // Load environment variables from .env file
 dotenv.config();
 
@@ -16,11 +16,13 @@ mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
     // Create the Express app
     const app = express();
 
+    app.use(express.json());
     // Middleware to parse JSON
     //app.use(express.json());
 
-    // Use the UserRoute for /api/user path
+     
     app.use('/api/user', UserRouter);
+    app.use('/api/auth', authRouter);
 
     // Start the server
     const PORT = process.env.PORT || 3000;
